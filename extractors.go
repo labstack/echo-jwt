0391aa42d2cbd978c24c24b2cced2879d6151b13
toimtoimtoim <desinformatsioon@gmail.com>
2023-01-26 22:12:52 +0200

    Add TokenExtractionError and TokenParsingError types to help distinguishing error source in ErrorHandler

--- extractors.go
+++ extractors.go
@@ -18,6 +18,19 @@ const (
 	extractorLimit = 20
 )
 
+// TokenExtractionError is catch all type for all errors that occur when the token is extracted from the request. This
+// helps to distinguish extractor errors from token parsing errors even if custom extractors or token parsing functions
+// are being used that have their own custom errors.
+type TokenExtractionError struct {
+	Err error
+}
+
+// Is checks if target error is same as TokenExtractionError
+func (e TokenExtractionError) Is(target error) bool { return target == ErrJWTMissing } // to provide some compatibility with older error handling logic
+
+func (e *TokenExtractionError) Error() string { return e.Err.Error() }
+func (e *TokenExtractionError) Unwrap() error { return e.Err }
+
 var errHeaderExtractorValueMissing = errors.New("missing value in request header")
 var errHeaderExtractorValueInvalid = errors.New("invalid value in request header")
 var errQueryExtractorValueMissing = errors.New("missing value in the query string")
